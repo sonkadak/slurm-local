c5ef7aa2bd866ccd837b0d7bd7064e7ddc7c60bf
sonkadak <jhy070997@naver.com>
2025-05-23 06:38:27 +0000

    feat(script/setup): Update nfs setup scripts
    Update setup scripts for nfs
    - Update nfs repo directory structure
    - Update apt indexing

--- setup/scripts/setup-nfs-repo.sh
+++ setup/scripts/setup-nfs-repo.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-#REPO_ROOT="./offline_repo"
-REPO_ROOT="./offline_nfs"
+REPO_ROOT=(offline_nfs offline_repo)
 
-find "$REPO_ROOT" -type d | while read -r dir; do
-  # .deb 파일이 있는 디렉토리만 처리
-  shopt -s nullglob
-  debs=("$dir"/*.deb)
-  if [ ${#debs[@]} -eq 0 ]; then
-    continue
-  fi
+for target in "${REPO_ROOT[@]}"; do
+  find "$target" -type d | while read -r dir; do
+    # .deb 파일이 있는 디렉토리만 처리
+    shopt -s nullglob
+    debs=("$dir"/*.deb)
+    if [ ${#debs[@]} -eq 0 ]; then
+      continue
+    fi
 
-  echo "📦 Create index in: $dir"
-  dpkg-scanpackages "$dir" /dev/null | tee "$dir/Packages" | gzip -9c > "$dir/Packages.gz"
+    echo "📦 Create index in: $dir"
+    dpkg-scanpackages "$dir" /dev/null | tee "$dir/Packages" | gzip -9c > "$dir/Packages.gz"
+  done
 done
 
 echo "✅ Local APT indexes created successfully."
